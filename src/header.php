df8eb55061696850bb3bba027372851a2bf02fc5
Mubelotix <mubelotix@gmail.com>
2022-08-26 11:24:56 +0200

    Responsive header

--- src/header.php
+++ src/header.php
@@ -101,13 +101,56 @@
 			width: 2rem;
 			height: 2rem;
             z-index:5;
-		} 
+		}
+
+		@media all and (max-width: 1200px) {
+			#header > * {
+				margin: 0 .75rem;
+			}
+
+			#header > #sitename {
+				margin-right: 1.5rem;
+				font-size: 1.5rem;
+			}
+
+			#header-flag, #header-account {
+				margin: 0 .5rem;
+			}
+
+			#header-right-part {
+				margin-right: unset;
+			}
+		}
+
+		@media all and (max-width: 850px) {
+			#header > a:nth-child(2) {
+				display: none;
+			}
+		}
+
+		@media all and (max-width: 750px) {
+			#header > a:nth-child(4) {
+				display: none;
+			}
+		}
+
+		@media all and (max-width: 650px) {
+			#header > a:nth-child(5) {
+				display: none;
+			}
+		}
+
+		@media all and (max-width: 550px) {
+			#header > a:nth-child(3) {
+				display: none;
+			}
+		}
 	</style>
     <div id="sitename">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket3-fill" viewBox="0 0 16 16">
             <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z"/>
         </svg>
-        No More Waste
+        No&nbsp;More&nbsp;Waste
     </div>
     <a href="/PA/<?php echo $language ?>/">Accueil</a>
     <?php
