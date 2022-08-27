154913f387f0cf3eecbfdc755c30079f623d5626
Marina <marina.robert1508@gmail.com>
2022-08-27 11:38:19 +0200

    style

--- src/stock.php
+++ src/stock.php
@@ -1,19 +1,112 @@
 <?php include __DIR__ . "\header.php";?>
 
 <main>
+    <style type="text/css">
+        main {
+            padding-top: 3rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+        }
 
+        form {
+            width: 60vw;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        table {
+            table-layout: fixed;
+            margin-top:20px;
+        }
+
+        #products td>input {
+            min-width: calc(100% - .8rem);
+            max-width: calc(100% - .8rem);
+        }
+
+        /* Borders */
+        #table td, #table th {
+            border: 1px solid black;
+            padding: 0;
+        }
+
+        #table th {
+            padding: .2rem;
+        }
+        #table td:nth-child(2), #table th:nth-child(2) {
+            border-left: none;
+        }
+        #table td:nth-child(1), #table th:nth-child(1) {
+            border-right: none;
+            width: 2rem;
+        }
+        #table td {
+            padding: .4rem;
+        }
+
+        /* Cell sizes */
+        #table th:nth-child(2) {
+            width: calc(75%);
+        }
+        #table th:nth-child(3) {
+            width: calc(35%);
+        }
+
+        .btn {
+            margin-top: 2.5%;
+            background-color: #6EB784FF;
+            width: 10rem;
+        }
+
+        .btn:hover{
+            background-color: #518863;
+            border-color: #113b1a!important;
+        }
+
+        .btn:focus{
+            box-shadow: 0 0 0 0.2rem rgb(97, 159, 114);
+        }
+
+        td svg {
+            margin-bottom: 5%;
+            margin-left: 5%;
+        }
+
+        /* Paragraph */
+        main>p {
+            font-size: 1.5rem;
+            width: 100%;
+            text-align: center;
+            margin-bottom: 1.5rem;
+        }
+        form>p, #informations>p {
+            width: 100%;
+            text-align: left;
+            margin-bottom: .5rem;
+        }
+
+        .table-striped td {
+            overflow: auto;
+        }
+
+        /* Responsiveness */
+        @media all and (max-width: 800px) {
+            form {
+                width: 95vw;
+            }
+        }
+    </style>
 
     <h1>Stock</h1>
 
-    <input type="text" value="" name="search" id="search">
+    <input style="width:60vw" type="text" value="" name="search" id="search" placeholder="Rechercher un code barre ...">
 
     <div id="products">
 
     </div>
-    <!--
-        <form id="form" class="formulaire" action="distribution" method="POST" enctype='multipart/form-data'>
-
-        </form>-->
 
     <form id="form" class="formulaire" action="distribution" method="POST" enctype='multipart/form-data'>
         <table class="table table-striped table-hover" id="table">
@@ -25,7 +118,7 @@
         </table>
     </form>
 
-    <button onclick="createDistrib()">Créer une nouvelle distribution</button>
+    <button onclick="createDistrib()" class="btn">Créer une nouvelle distribution</button>
 
     <script>
         var products = [];
@@ -73,37 +166,33 @@
                         let product_code = response['stock'][i]['product_code'];
                         let reg = new RegExp("^" + ev);
                         if (reg.test(product_code)) {
-                            /*
-                            let product = response['stock'][i];
-                            let productDiv = document.createElement('div');
-                            productDiv.id = 'product';
-                            let productInfo = document.createElement('div');
-                            productInfo.id = "product-info";
-                            productDiv.innerHTML = '<input type="checkbox" id="select' + i + '">';
-                            productDiv.innerHTML += '<div id="product_code' + i +'">' + product['product_code'] + '</div>';
-                            productDiv.innerHTML += '<div>' + product['quantity'] + '</div>';
-
-                            productDiv.appendChild(productInfo);
-                            document.getElementById("products").appendChild(productDiv);
-*/
                             let table = document.getElementById("table");
-                            console.log(table);
                             let product = response['stock'][i];
-                            table.innerHTML += '<tr id="product"><td><input type="checkbox" id="select' + i + '"></td><td id="product_code' + i +'">' + product['product_code'] + '</td><td>' + product['quantity'] + '</td></tr>';
-
+                            let newTr = document.createElement('tr');
+                            newTr.id = 'product';
+                            let newTd = document.createElement('td');
+                            let newInput = document.createElement('input');
+                            newInput.type = "checkbox";
+                            newInput.id = "select" + i;
+                            newTd.appendChild(newInput);
+                            newTr.appendChild(newTd);
+                            newTd = document.createElement('td');
+                            newTd.id = "product_code" + i;
+                            newTd.innerHTML = product['product_code'];
+                            newTr.appendChild(newTd);
+                            newTd = document.createElement('td');
+                            newTd.innerHTML = product['quantity'];
+                            newTr.appendChild(newTd);
+                            table.appendChild(newTr);
 
                             for (let j = 0; j < products.length; j++) {
-                                console.log(products);
                                 if (products[j] == product_code) {
                                     document.getElementById("select" + i).checked = true;
                                     break;
                                 }
                             }
-                            console.log(i)
-                            console.log(document.getElementById("select" + i));
-                            document.getElementById("select" + i).addEventListener('change', (event) => {
 
-                                console.log(products)
+                            document.getElementById("select" + i).addEventListener('change', (event) => {
                                 if (event.target.checked) {
                                         products.push(product_code);
                                     } else {
